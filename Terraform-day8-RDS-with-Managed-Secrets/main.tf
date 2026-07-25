62c67a13732311f338c3babc645cabbb26924b64
akuriprabh <prabhu.akuri1998@gmail.com>
2026-07-24 22:18:54 -0500

    day8

---
+++ Terraform-day8-RDS-with-Managed-Secrets/main.tf
@@ -0,0 +1,73 @@
+# VPC creation
+resource "aws_vpc" "name" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Prabhu's"
+  }
+}
+
+# subnet1 creation
+resource "aws_subnet" "name" {
+  vpc_id            = aws_vpc.name.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+# subnet2 creation
+resource "aws_subnet" "name2" {
+  vpc_id            = aws_vpc.name.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+}
+
+# subnet group creation for RDS
+resource "aws_db_subnet_group" "name" {
+  name       = "my-db-subnet-group"
+  subnet_ids = [aws_subnet.name.id, aws_subnet.name2.id]
+}
+
+# Security group creation
+resource "aws_security_group" "name" {
+  name        = "my-db-security-group"
+  description = "Allow MySQL traffic"
+  vpc_id      = aws_vpc.name.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# RDS creation
+resource "aws_db_instance" "name" {
+  identifier             = "my-rds-instance"
+  allocated_storage      = 20
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  db_subnet_group_name   = aws_db_subnet_group.name.name
+  vpc_security_group_ids = [aws_security_group.name.id]
+  skip_final_snapshot    = true
+  username               = "admin"
+  password               = var.db_password
+  #manage_master_user_password = true #enable password management by AWS Secrets Manager
+  maintenance_window     = "Mon:00:00-Mon:03:00"
+  backup_window          = "03:00-06:00"
+
+
+}
+
